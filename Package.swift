cdb6ea32567fec99d0efc26a79e67aa07f16103e
George King <george@theorypractice.dev>
2021-12-11 09:27:46 -0500

    Package.swift: update to swift 5.5.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // Dedicated to the public domain under CC0: https://creativecommons.org/publicdomain/zero/1.0/.
 
 
@@ -12,9 +12,9 @@ let package = Package(
     .executable(name: "zapple", targets: ["zapple"]),
   ],
   targets: [
-    .target(name: "del", path: "src/del"),
-    .target(name: "gen-thumbnails", path: "src/gen-thumbnails"),
-    .target(name: "zapple", path: "src/zapple"),
+    .executableTarget(name: "del", path: "src/del"),
+    .executableTarget(name: "gen-thumbnails", path: "src/gen-thumbnails"),
+    .executableTarget(name: "zapple", path: "src/zapple"),
   ],
   swiftLanguageVersions: [.v5]
 )
